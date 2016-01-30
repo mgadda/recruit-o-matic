57ef6b1df6c66c055fa93c292e81db2171a387d8
Matt Gadda <mgadda@gmail.com>
2016-01-30 10:52:09 -0800

    cleanup unused code

--- recruit.js
+++ recruit.js
@@ -1,33 +1,3 @@
-
-// Adapted from http://codepen.io/nottrobin/pen/meObWe
-// var copyBtn = document.querySelector('#copy-btn');
-// copyBtn.addEventListener('click', function () {
-//   // Select the email link anchor text
-//   var messageEl = document.querySelector('.message');
-//   var range = document.createRange();
-//   range.selectNode(messageEl);
-//   window.getSelection().addRange(range);
-
-//   try {
-//     // Now that we've selected the anchor text, execute the copy command
-//     document.execCommand('copy');
-//     // track copy button click event
-//     if (typeof(ga) !== 'undefined') {
-//       ga('send', 'event', {
-//         eventCategory: 'Button',
-//         eventAction: 'click',
-//         eventLabel: window.location.hash.slice(1)
-//       });
-//     }
-//   } catch(err) {
-//     console.log('Oops, unable to copy.');
-//   }
-
-//   window.getSelection().removeAllRanges();
-
-//   copyBtn.textContent = "Copied!";
-// });
-
 function recruitOmatic(paragraphLengths, prng) {
   var chain = new Chain(chainData, prng);
   var message = document.querySelector(".message");
@@ -67,10 +37,6 @@ function init() {
 
   var moreBtn = document.querySelector('#more-btn');
   moreBtn.addEventListener('click', function () {
-    // Reset button state
-    //var copyBtn = document.querySelector('#copy-btn');
-    //copyBtn.textContent = "Copy to clipboard";
-
     // Update hash with last seed to this is bookmark-able
     window.location.hash = prng.getSeed();
     recruitOmatic([1,3,3,1], prng);
@@ -83,18 +49,6 @@ function init() {
   });
 
   updateTweetButton();
-
-  //twttr.ready(function () {
-  //  twttr.widgets.createShareButton(
-  //    'https:\/\/recruit-o-matic.com\/',
-  //    document.getElementById('share'),
-  //    {
-  //      hashtags: "recruit-o-matic",
-  //      size: "large"
-  //    }
-  //  );
-  //});
-
 }
 
 document.onreadystatechange = function () {
